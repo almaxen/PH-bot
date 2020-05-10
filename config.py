a75035aa9439fdaa79d62c9f1d0a06cf41ce484a
Alice <yagomush@gmail.com>
2020-05-11 02:35:53 +0300

    last

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-TOKEN = '1194351305:AAE-FFvnKn7pFO8FLKN0ajtYd0PlAkzJWDY'
+TOKEN = '1194351305:AAFrmarhK1jThb6iVDi5SjnSkOCtF1B6Q-0'
 APP_NAME = 'prbot1'
--- myBot.py
+++ myBot.py
@@ -6,7 +6,7 @@ import telebot
 import logging
 import os
 from telebot import types
-from flask import Flask, request
+# from flask import Flask, request
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -22,54 +22,51 @@ def send_welcome(message):
 @bot.message_handler(commands=['enter'])
 def command_enter(message):
     markup = types.InlineKeyboardMarkup()
-    bot.send_message(message.chat.id, text='Твой Председатель: у тебя что-то срочное?', reply_markup=markup)
     button1 = types.InlineKeyboardButton('Да', callback_data="b1")
     button2 = types.InlineKeyboardButton('Пойдем курить', callback_data="b2")
     button3 = types.InlineKeyboardButton('Куда положить печенье?', callback_data="b3")
     button4 = types.InlineKeyboardButton('Как принимать матпомощь?', callback_data="b4")
     button5 = types.InlineKeyboardButton('* молча вышел *', callback_data="b5")
     markup.add(button1, button2, button3, button4, button5)
+    bot.send_message(message.chat.id, text='Твой Председатель: у тебя что-то срочное?', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    try:
-        if call.message:
-            if call.data == "b1":
-                bot.send_message(call.message.from_user.id, text='Слушаю', reply_markup=None)
-            if call.data == "b2":
-                bot.send_message(call.message.from_user.id, text='Стартуем', reply_markup=None)
-            if call.data == "b3":
-                bot.send_message(call.message.from_user.id, text='* BOOM *', reply_markup=None)
-            if call.data == "b4":
-                bot.send_message(call.message.from_user.id, text='К Оле Гончаровой', reply_markup=None)
-            if call.data == "b5":
-                bot.send_message(call.message.from_user.id, text='У нас так не принято', reply_markup=None)
-    except Exception as e:
-        print(repr(e))
+    if call.message:
+        if call.data == "b1":
+            bot.send_message(call.message.chat.id, text='Слушаю', reply_markup=None)
+        elif call.data == "b2":
+            bot.send_message(call.message.chat.id, text='Стартуем', reply_markup=None)
+        elif call.data == "b3":
+            bot.send_message(call.message.chat.id, text='* BOOM *', reply_markup=None)
+        elif call.data == "b4":
+            bot.send_message(call.message.chat.id, text='К Оле Гончаровой', reply_markup=None)
+        elif call.data == "b5":
+            bot.send_message(call.message.chat.id, text='У нас так не принято', reply_markup=None)
 
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
+#if "HEROKU" in list(os.environ.keys()):
+   # logger = telebot.logger
+   # telebot.logger.setLevel(logging.INFO)
 
-    server = Flask(__name__)
+   # server = Flask(__name__)
 
-    @server.route('/bot', methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+  #  @server.route('/bot', methods=['POST'])
+   # def getMessage():
+        #bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+      #  return "!", 200
+#
+  #  @server.route("/")
+  # def webhook():
+    #    bot.remove_webhook()
+      #  bot.set_webhook(url='https://dashboard.heroku.com/apps/prbot1')
+      #  return "?", 200
+    #server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  # либо порт 80
 
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url='https://dashboard.heroku.com/apps/prbot1')
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  # либо порт 80
-
-else:
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+# else:
+   # bot.remove_webhook()
+   # bot.polling(none_stop=True)
 
 # if __name__ == '__main__':
     # server.debug = True
@@ -77,6 +74,6 @@ else:
 
 
 
-# if __name__ == '__main__':
-#    bot.polling(none_stop=True)
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
 
